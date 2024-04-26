52960d3d642cfb9b3b24f42d0c667492f7363ad1
ElectricBeat <96882916+ElectricBeat@users.noreply.github.com>
2024-04-26 14:36:00 +0530

    VAS-AVR-ATmega16-2-b-LCD-4-BIT

---
+++ CODE/LCD 4 BIT/LCD 4 BIT/main.c
@@ -0,0 +1,24 @@
+/*
+ * LCD 4 BIT.c
+ *
+ * Created: 25-04-2024 17:51:16
+ * Author : SRIDHAR
+ */
+
+#define F_CPU 1000000UL					/* Define CPU Frequency e.g. here 8MHz */
+#include <avr/io.h>						/* Include AVR std. library file */
+#include <util/delay.h>					/* Include inbuilt defined Delay header file */
+#include "lcd_4_bit.h"
+
+int main()
+{
+	LCD_Init();							/* Initialize LCD */
+
+	while (1)
+	{
+		lcd_cursor(1,0);				/* Go to 1st line*/
+		LCD_String("VAS");				/* write string on 1st line of LCD*/
+		LCD_Command (0x1C);
+		_delay_ms(100);
+	}
+}
